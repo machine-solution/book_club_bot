63ef9603a7723a443ea7f08f4eb75179c9f617e0
machine-solution <machine.solution353@gmail.com>
2023-07-19 21:15:00 +0300

    change default message

--- vk_bot/bot.py
+++ vk_bot/bot.py
@@ -3,14 +3,18 @@ import os
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+import const
+
 load_dotenv()
 TOKEN = os.environ["VK_TOKEN"]
 
 vk_session = vk_api.VkApi(token = TOKEN)
 longpoll = VkLongPoll(vk_session)
 
+
 def send_message(id, text):
     vk_session.method('messages.send', {'user_id' : id, 'message' : text, 'random_id': 0})
+
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
@@ -18,11 +22,4 @@ for event in longpoll.listen():
             message = event.text.lower()
             id = event.user_id
 
-            if message == 'привет':
-                send_message(id, 'Привет, я бот!')
-
-            elif message == 'как дела?':
-                send_message(id, 'Хорошо, а твои как?' )
-
-            else:
-                send_message(id, 'Я вас не понимаю! :(')
+            send_message(id, const.DEFAULT_REPLY)
