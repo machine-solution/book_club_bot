cf56a58001d40a29bfcfed7dc998bc5fb4a6f0ae
machine-solution <machine.solution353@gmail.com>
2023-12-26 21:02:52 +0300

    add button

--- vk_bot/bot.py
+++ vk_bot/bot.py
@@ -1,30 +1,84 @@
-from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
+import os
+
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+# from vk_api.longpoll import VkLongPoll, VkEventType
 
 from vk_bot import const
 from vk_bot import vk
+import json
 
+load_dotenv()
+VK_GROUP_ID = os.environ["VK_GROUP_ID"]
 
 session = vk.get_session()
-longpoll = VkLongPoll(session)
+bot_longpoll = VkBotLongPoll(session, VK_GROUP_ID)
+# longpoll = VkLongPoll(session)
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            message = event.text.lower()
-            vk_id = event.user_id
-            vk_tag = vk.get_vk_tag(
-                session=session,
-                vk_id=vk_id,
-            )
-            user = vk.get_user_by_vk_id(
-                session=session,
-                vk_id=vk_id,
-            )
-            name = "@" + user["vk_tag"] if user else "Незнакомец"
+for event in bot_longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        vk_id = event.obj.message["from_id"]
+        vk_tag = vk.get_vk_tag(
+            session=session,
+            vk_id=vk_id,
+        )
+        user = vk.get_user_by_vk_id(
+            session=session,
+            vk_id=vk_id,
+        )
+        name = "@" + user["vk_tag"] if user else "Незнакомец"
 
-            vk.send_message(
-                session=session,
-                vk_id=vk_id,
-                text=const.REPLY_WITH_NAME % name,
-            )
+        keyboard=json.dumps({
+            "one_time": False,
+            "buttons": [
+                [
+                    {
+                        "action": {
+                            "type": "callback",
+                            "payload": json.dumps({
+                                "type": "custom",
+                                "state": "1",
+                            }),
+                            "label": "Вступить в клуб",
+                        },
+                        "color": "secondary",
+                    },
+                ],
+            ]
+        })
+
+        vk.send_message(
+            session=session,
+            vk_id=vk_id,
+            text=const.REPLY_WITH_NAME % name,
+            keyboard=keyboard,
+        )
+    elif event.type == VkBotEventType.MESSAGE_EVENT:
+        vk_id = event.obj.user_id
+        if event.obj.payload.get("type", "") != "custom" or event.obj.payload.get("state", "") != "1":
+            continue
+        user_id = vk.register_user(
+            session=session,
+            vk_id=vk_id,
+        )
+        if user_id is None:
+            message = "Вы уже были зарегистрированы ранее"
+        else:
+            message = "Вы зарегистрированы под id %s" % user_id
+        vk.answer_event(
+            session=session,
+            vk_id=vk_id,
+            peer_id=event.obj.peer_id,
+            event_id=event.obj.event_id,
+        )
+        keyboard=json.dumps({
+            "one_time": False,
+            "buttons": [],
+        })
+        vk.send_message(
+            session=session,
+            vk_id=vk_id,
+            text=message,
+            keyboard=keyboard,
+        )
--- vk_bot/vk.py
+++ vk_bot/vk.py
@@ -20,16 +20,39 @@ def get_api():
 
 
 def method(session, name, **kwargs):
-    kwargs['random_id'] = vk_utils.get_random_id()
+    kwargs["random_id"] = vk_utils.get_random_id()
     return session.method(name, kwargs)
 
 
-def send_message(session, vk_id, text) -> None:
-    method(
+def send_message(session, vk_id, text, keyboard=None) -> tp.Optional[tp.Dict]:
+    return method(
         session=session,
-        name='messages.send',
+        name="messages.send",
         user_id=vk_id,
-        message=text
+        message=text,
+        keyboard=keyboard,
+    )
+
+
+def edit_message(session, peer_id, conversation_message_id, text, keyboard=None) -> tp.Optional[tp.Dict]:
+    return method(
+        session=session,
+        name="messages.edit",
+        conversation_message_id=conversation_message_id,
+        peer_id=peer_id,
+        message=text,
+        keyboard=keyboard,
+    )
+
+
+def answer_event(session, vk_id, peer_id, event_id, event_data = None) -> tp.Optional[tp.Dict]:
+    return method(
+        session=session,
+        name="messages.sendMessageEventAnswer",
+        user_id=vk_id,
+        peer_id=peer_id,
+        event_id=event_id,
+        event_data=event_data,
     )
 
 
@@ -37,14 +60,15 @@ def get_vk_tag(session, vk_id) -> tp.Optional[str]:
     try:
         users_data = method(
             session=session,
-            name='users.get',
+            name="users.get",
             user_ids=[vk_id],
-            fields=['screen_name'],
+            fields=["screen_name"],
         )
     except:
         return None
-    user_data = users_data[0]
-    return user_data['screen_name']
+    if not users_data:
+        return None
+    return users_data[0]["screen_name"]
 
 
 def get_user_by_vk_id(session, vk_id) -> tp.Optional[tp.Dict]:
@@ -75,4 +99,4 @@ def register_user(session, vk_id) -> tp.Optional[int]:
     )
     if result is None:
         return None
-    return result['user_id']
+    return result["user_id"]
