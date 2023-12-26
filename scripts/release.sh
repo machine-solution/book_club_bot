9a208d280ab57571638e7c81dd5a086b288f2960
machine-solution <machine.solution353@gmail.com>
2023-12-26 15:14:28 +0300

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,8 +4,17 @@ cd /home/machine_solution/book_club/book_club_bot/
 
 echo "$(date +%F::%T) Start release" >> /home/machine_solution/logs/shell.log
 
-scripts/update.sh &&
-sudo systemctl restart script >> /home/machine_solution/logs/shell.log
+scripts/update.sh >> /home/machine_solution/logs/shell.log
+
+code=$?
+
+if [[ $code == 0 ]]; then
+    source venv/bin/activate
+    python -m pip install -r requirements.txt &&
+    sudo systemctl restart script >> /home/machine_solution/logs/shell.log
+else
+    exit $code
+fi
 
 code=$?
 
